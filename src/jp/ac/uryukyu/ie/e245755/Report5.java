59c8403c8223201c9d656832ba8f81b90b6a4381
Minami UTO <e245755@ie.u-ryukyu.ac.jp>
2024-12-13 17:07:55 +0900

    first commit

---
+++ src/jp/ac/uryukyu/ie/e245755/Report5.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e245755;
+
+public class Report5 {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.length());
+    }
+}
