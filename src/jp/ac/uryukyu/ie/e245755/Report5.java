2f620d2618ad132e8b1aadeffebdc84d8b55144c
Minami UTO <e245755@ie.u-ryukyu.ac.jp>
2024-12-15 13:47:56 +0900

    third

--- src/jp/ac/uryukyu/ie/e245755/Report5.java
+++ src/jp/ac/uryukyu/ie/e245755/Report5.java
@@ -3,8 +3,9 @@ package jp.ac.uryukyu.ie.e245755;
 public class Report5 {
     public static void main(String[] args) {
         try{
-            String str = null;
-            System.out.println(str.length());
+            String str = "壱百満";
+            int value = Integer.parseInt(str);
+            System.out.println(value);
         } catch (NullPointerException e){
             System.out.println("NullPointerExceptionが発生!");
             System.out.println(e.getMessage());
